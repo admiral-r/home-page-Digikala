60f86d6711847a02e0c44be799c25aa4e45fea50
admiral <amirrajabzade.ir@gmail.com>
2022-07-19 15:26:16 +0430

    The whole project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'digikala';
+  imgNumber:number = 1;
+  img:string = '../assets/img/bg-banner' + this.imgNumber + '.jpg';
+  imgNum:number = 1;
+
+  prevSlides() {
+    if(this.imgNum == 1) {
+      this.imgNum = 6; 
+    }
+    this.imgNum -= this.imgNumber;
+    this.img = '../assets/img/bg-banner' + this.imgNum + '.jpg';
+  }
+  nextSlides() {
+    if(this.imgNum == 5) {
+      this.imgNum = 0; 
+    }
+    this.imgNum += this.imgNumber;
+    this.img = '../assets/img/bg-banner' + this.imgNum + '.jpg';
+  }
+  backToTop() {
+    window.scroll({ 
+            top: 0
+     });
+ }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+
+
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
